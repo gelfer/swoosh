36640cdafb81bf12424c01a98592dbf99e67f49a
Gelfer <gelferstone@gmail.com>
2019-05-31 20:29:16 +0700

    lesson 7

--- app/src/main/java/darkbluestone/com/swoosh/SkillActivity.kt
+++ app/src/main/java/darkbluestone/com/swoosh/SkillActivity.kt
@@ -3,10 +3,15 @@ package darkbluestone.com.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        var league = ""
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+        league = intent.getStringExtra(EXTRA_LEAGUE)
+        println(league)
     }
 }
